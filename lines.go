839aeac687c3ee424f398a03b7e69d9dce183867
Jacob Alzen <jacob.alzen@gmail.com>
2021-12-13 19:16:11 +0100

    Initial star for "julstugan" with Opera 2021
    A competition to create a christmas star for Opera.

--- lines.go
+++ lines.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image/color"
 	"math"
 
 	"fyne.io/fyne/v2"
@@ -29,24 +30,29 @@ func NewLineDrawer(matrix matrix.Matrix) *LineDrawer {
 // LinesFromMatrix creates new canvas.Line from the matrix.
 func LinesFromMatrix(M matrix.Matrix) []fyne.CanvasObject {
 	return []fyne.CanvasObject{
-		NewLineBetween(M[0][0], M[0][1], M[1][0], M[1][1]),
-		NewLineBetween(M[0][1], M[0][2], M[1][1], M[1][2]),
-		NewLineBetween(M[0][2], M[0][3], M[1][2], M[1][3]),
-		NewLineBetween(M[0][3], M[0][4], M[1][3], M[1][4]),
-		NewLineBetween(M[0][4], M[0][5], M[1][4], M[1][5]),
-		NewLineBetween(M[0][5], M[0][0], M[1][5], M[1][0]),
-		NewLineBetween(M[0][6], M[0][7], M[1][6], M[1][7]),
-		NewLineBetween(M[0][7], M[0][8], M[1][7], M[1][8]),
-		NewLineBetween(M[0][8], M[0][9], M[1][8], M[1][9]),
-		NewLineBetween(M[0][9], M[0][10], M[1][9], M[1][10]),
-		NewLineBetween(M[0][10], M[0][11], M[1][10], M[1][11]),
-		NewLineBetween(M[0][11], M[0][6], M[1][11], M[1][6]),
-		NewLineBetween(M[0][0], M[0][6], M[1][0], M[1][6]),
-		NewLineBetween(M[0][1], M[0][7], M[1][1], M[1][7]),
+		NewLineBetween(M[0][0], M[0][12], M[1][0], M[1][12]),
+		NewLineBetween(M[0][0], M[0][10], M[1][0], M[1][10]),
+
+		NewLineBetween(M[0][1], M[0][10], M[1][1], M[1][10]),
+		NewLineBetween(M[0][1], M[0][8], M[1][1], M[1][8]),
+
+		NewLineBetween(M[0][2], M[0][9], M[1][2], M[1][9]),
 		NewLineBetween(M[0][2], M[0][8], M[1][2], M[1][8]),
+
+		NewLineBetween(M[0][3], M[0][11], M[1][3], M[1][11]),
 		NewLineBetween(M[0][3], M[0][9], M[1][3], M[1][9]),
-		NewLineBetween(M[0][4], M[0][10], M[1][4], M[1][10]),
-		NewLineBetween(M[0][5], M[0][11], M[1][5], M[1][11]),
+
+		NewLineBetween(M[0][4], M[0][13], M[1][4], M[1][13]),
+		NewLineBetween(M[0][4], M[0][11], M[1][4], M[1][11]),
+
+		NewLineBetween(M[0][5], M[0][15], M[1][5], M[1][15]),
+		NewLineBetween(M[0][5], M[0][13], M[1][5], M[1][13]),
+
+		NewLineBetween(M[0][6], M[0][14], M[1][6], M[1][14]),
+		NewLineBetween(M[0][6], M[0][15], M[1][6], M[1][15]),
+
+		NewLineBetween(M[0][7], M[0][12], M[1][7], M[1][12]),
+		NewLineBetween(M[0][7], M[0][14], M[1][7], M[1][14]),
 	}
 }
 
@@ -120,9 +126,9 @@ func (lr *lineRenderer) Refresh() {
 // NewLineBetween creates a new line between the given coordinates.
 func NewLineBetween(x1, y1, x2, y2 float64) *canvas.Line {
 	return &canvas.Line{
-		Position1:   fyne.NewPos(float32(x1)+300, float32(x2)+300),
-		Position2:   fyne.NewPos(float32(y1)+300, float32(y2)+300),
-		StrokeColor: theme.PrimaryColor(),
-		StrokeWidth: 3,
+		Position1:   fyne.NewPos(float32(x1)+20, float32(x2)+20),
+		Position2:   fyne.NewPos(float32(y1)+20, float32(y2)+20),
+		StrokeColor: color.NRGBA{R: 0xff, G: 0xeb, B: 0x3b, A: 0xaf},
+		StrokeWidth: 7,
 	}
 }
--- main.go
+++ main.go
@@ -9,30 +9,32 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Linear Algebra Graphics")
+	w := a.NewWindow("Opera ❤️  Linear Algebra")
 
-	// These are actually points, but use vectors for simplicity.
-	p1 := rn.VecN{1.000, -0.800, 0.000}
-	p2 := rn.VecN{0.500, -0.800, -0.866}
-	p3 := rn.VecN{-0.500, -0.800, -0.866}
-	p4 := rn.VecN{-1.000, -0.800, 0.000}
-	p5 := rn.VecN{-0.500, -0.800, 0.866}
-	p6 := rn.VecN{0.500, -0.800, 0.866}
-	p7 := rn.VecN{0.840, -0.400, 0.000}
-	p8 := rn.VecN{0.315, 0.125, -0.546}
-	p9 := rn.VecN{-0.210, 0.650, -0.364}
-	p10 := rn.VecN{-0.360, 0.800, 0.000}
-	p11 := rn.VecN{-0.210, 0.650, 0.364}
-	p12 := rn.VecN{0.315, 0.125, 0.546}
+	// Outside points:
+	p1 := rn.VecN{0.0, 6.5, 0}
+	p2 := rn.VecN{3.0, 3.0, 0}
+	p3 := rn.VecN{7.0, 0.0, 0}
+	p4 := rn.VecN{10.0, 3.0, 0}
+	p5 := rn.VecN{14.0, 6.5, 0}
+	p6 := rn.VecN{10.0, 10.0, 0}
+	p7 := rn.VecN{7.0, 13.0, 0}
+	p8 := rn.VecN{3.0, 10.0, 0}
 
-	M := matrix.NewFromVec(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12)
+	// Inside points:
+	p9 := rn.VecN{6.0, 5.0, 0}
+	p10 := rn.VecN{8.0, 5.0, 0}
+	p11 := rn.VecN{5.0, 6.0, 0}
+	p12 := rn.VecN{9.0, 6.0, 0}
+	p13 := rn.VecN{5.0, 7.0, 0}
+	p14 := rn.VecN{9.0, 7.0, 0}
+	p15 := rn.VecN{6.0, 8.0, 0}
+	p16 := rn.VecN{8.0, 8.0, 0}
 
-	// Transpose by u in specific directions.
-	//u := rn.VecN{2, 1, 0}
-	//M, _ = matrix.AddVec(M, u)
+	M := matrix.NewFromVec(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16)
 
 	// Scale by a factor s in all directions.
-	s := float64(200)
+	s := float64(40)
 	T := matrix.Matrix{
 		{s, 0, 0},
 		{0, s, 0},
